375e0b890fc097acdf4e465b9275df2880b0637f
Ben <Benjamin.Bigelow@kearsarge.org>
2016-12-14 12:01:17 -0500

    OMG it actually does something

--- Game.py
+++ Game.py
@@ -1,1 +1,37 @@
+import pygame, sys, math, random
 
+from Cheese import *
+from Pac import *
+from PlayerPac import *
+from Wall import *
+from Level import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 800 
+height = 600
+size = width, height
+screen = pygame.display.set_mode(size)
+
+bgColor = r,g,b = 67, 75, 198
+
+pacs = [Pac("RedTestPac.png", [1,1], [width/2, height/2]),
+        Pac("RedTestPac.png", [-1,-1], [width/3, height/3])]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        
+    for pac in pacs:
+        pac.move()
+        pac.bounceScreen(size)
+        
+    bgColor = r,g,b
+    screen.fill(bgColor)
+    for pac in pacs:
+        screen.blit(pac.image, pac.rect)
+    pygame.display.flip()
+    clock.tick(60)
+    
