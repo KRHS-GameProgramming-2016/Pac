2b47ecdc9cf01fa2b74337e4397057f9671fe81f
AlexanderDiDominic <Alexander.DiDominic@kearsarge.org>
2017-02-10 10:37:53 -0500

    sa

--- Wall.py
+++ Wall.py
@@ -2,10 +2,10 @@ import pygame, sys, math
 
 class Wall():
     def __init__(self, pos=[0,0], size=None):
-        self.image = pygame.image.load("Resources\Wall\Dunanana_cheese_man.png")
-        if size:
+        self.image = pygame.image.load("Resources\Wall\frontend-large.png")
+        if size: 
             self.image = pygame.transform.scale(self.image, [size,size])
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center = po
         
         
     
