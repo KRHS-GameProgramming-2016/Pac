9f1c93e5d066901b86fb0f25dd2600c5bb3eba84
Chris Spooner <cspooner@kearsarge.org>
2017-02-10 10:43:06 -0500

    Typos fixed

--- Wall.py
+++ Wall.py
@@ -2,10 +2,10 @@ import pygame, sys, math
 
 class Wall():
     def __init__(self, pos=[0,0], size=None):
-        self.image = pygame.image.load("Resources\Wall\frontend-large.png")
+        self.image = pygame.image.load("Resources/Cheese/frontend-large.png")
         if size: 
             self.image = pygame.transform.scale(self.image, [size,size])
-        self.rect = self.image.get_rect(center = po
+        self.rect = self.image.get_rect(center = pos)
         
         
     
